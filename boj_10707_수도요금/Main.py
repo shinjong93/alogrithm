f63f3ef58e727d4186da534a98f8b52f7b9f00ef
신종혁 <shinjong9312@gmail.com>
2017-11-28 10:41:54 +0900

    2017-11-18 by python3

--- boj_10707_수도요금/Main.py
+++ boj_10707_수도요금/Main.py
@@ -1,5 +1,21 @@
 class main:
+	A = int(input())
+	B = int(input())
+	limit = int(input())
+	Add = int(input())
+	use = int(input())
 
+	a_total = A * use;
 
-if __name__ == "__main__"
+	if use < limit:
+		b_total = B
+	else:
+		b_total = B + Add * (use - limit)
+
+	if a_total < b_total:
+		print(a_total)
+	else:
+		print(b_total)
+
+if __name__ == "__main__":
 	main()
